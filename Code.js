6db7bd6ce8ce57a2c826f3003bc1001f4c302b61
kleonAnintika <76479315+kleonAnintika@users.noreply.github.com>
2021-10-11 14:15:17 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,17 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+Fill("white")
+textSize(15)
+text("C O T N I F O N ",700,150)
+fill("lightblue")
+text("Hint: Performs a particular task!!",700,170)
+
+Fill("white")
+textSize(15)
+text("A T A D A S B A ",100,310)
+fill("lightblue")
+text("Hint: Stores all information!!",100,330)
+}
---
+++ System.js
@@ -0,0 +1,18 @@
+class System{
+
+       constructor(){}
+
+        authenticate(actualCode,enteredCode){
+            textSize(50);
+            Fill("Black");
+            text(code,300,300)
+
+           if(actualCode===enteredCode.toUpperCase()){
+               return true
+           }else{
+               return false
+           }
+
+
+        }
+}
